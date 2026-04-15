f185d2ccc832a3cfb6a5d558f8c9a37731305398
yazan daba <yazandaba@outlook.com>
2026-04-15 20:40:56 +0400

    Add consteval as contextual keyword

--- src/Compilers/CSharp/Portable/Syntax/SyntaxKind.cs
+++ src/Compilers/CSharp/Portable/Syntax/SyntaxKind.cs
@@ -429,6 +429,8 @@ namespace Microsoft.CodeAnalysis.CSharp
         /// <summary>Represents <see langword="union"/>.</summary>
         [Experimental("RSEXPERIMENTAL006", UrlFormat = "https://github.com/dotnet/roslyn/issues/82567")]
         UnionKeyword = 8452,
+        [Experimental("RSEXPERIMENTAL007")]
+        ConstevalKeyword = 8453,
 
         // when adding a contextual keyword following functions must be adapted:
         // <see cref="SyntaxFacts.GetContextualKeywordKinds()"/>
